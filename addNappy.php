79a7da4ef960b848ec32189d69c1a3bb6f56b505
Rauldukner <kevindunne@outlook.com>
2014-05-29 13:19:04 +0100

    Update addNappy.php

--- addNappy.php
+++ addNappy.php
@@ -1,45 +1,27 @@
- <? session_start(); ?>
-<!doctype html>
-<html>
-<head>
-    <title>My Page</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css">
-     
-    <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
-    <script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
-   
-    
-</head>
-<body>
+<?php
+ session_start();
+$con=mysqli_connect("kevindunnedesignscom.ipagemysql.com","raulduke","sinead89","babydata");
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 
-<div data-role=page id="register">
-  <div data-role=header>
-    
-  </div>
+// escape variables for security
+$ntype = mysqli_real_escape_string($con, $_GET['ntype']);
+$ntime = mysqli_real_escape_string($con, $_GET['ntime']);
+$ndate = mysqli_real_escape_string($con, $_GET['ndate']);
+$observations = mysqli_real_escape_string($con, $_GET['observations']);
 
-  <div data-role=content>
-    
+$sql="INSERT INTO nappy (ntype, ntime, ndate, observations)
+VALUES ('$ntype', '$ntime', '$ndate', '$observations')";
 
-    <form method="post" action="registerMe.php">
-    
-    <span> Last name </span>
-    <input type=text id="lname" name="lname">
-    <span> First name </span>
-    <input type=text id="fname" name="fname">
-    <span> Child</span>
-    <input type=text id="child" name="child">
-    <span> Email</span>
-    <input type=text id="email" name="email">
-    <span> Username</span>
-    <input type=text id="uname" name="uname">
-    <span> Password</span>
-    <input type=password id="pword" name="pword">
-<br>
-    <input type="submit" name="submit value="Register"> 
-</form>
-  </div>
-</div>
+if (!mysqli_query($con,$sql)) {
+  die('Error: ' . mysqli_error($con));
+}
 
-</body>
-</html> 
+header("location:index2.php");
+
+mysqli_close($con);
+
+die();
+?>
